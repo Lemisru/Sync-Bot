89cba899af761d56375b6d94cf5301e0ba6b30b4
Lemisru <maromaksi10@gmail.com>
2024-04-18 12:07:58 +0300

    добавлена команда 'конфиг инфо', исправлены баги 'конфиг изменить'

--- vk_bot/handlers/admin.py
+++ vk_bot/handlers/admin.py
@@ -11,23 +11,31 @@ config = configparser.ConfigParser()
 
 admin_labeler = BotLabeler()
 
-@admin_labeler.message(CommandRule("конфиг", VK_PREFIXES, 3))
+@admin_labeler.message(CommandRule("конфиг инфо", VK_PREFIXES))
+async def reactions_handler(message):
+    config.read("config.ini")
+
+    answer = ""
+    for key in config["VK"]:
+        answer += f"{key}= {config["VK"][key]}\n"
+        print(answer)
+    await message.answer(answer, reply_to=isReplyTo(message.id))
+
+@admin_labeler.message(CommandRule("конфиг изменить", VK_PREFIXES, 2))
 async def reactions_handler(message, args: tuple):
-    if args[0] != "изменить":
-        return
-    
     admin.read("data/admins.ini")
     admin["ADMINS"]
 
     admin_IDes = [int(key) for key in admin["ADMINS"]]
     if message.from_id in admin_IDes:
         config.read("config.ini")
-        if args[1] in [key for key in config["VK"]]:
-            config["VK"][args[1]] = args[2]
+        if args[0] in [key for key in config["VK"]]:
+            config["VK"][args[0]] = args[1]
+
+            with open('config.ini', 'w') as configfile:
+                config.write(configfile)
+            await message.answer("Конфиг успешно изменён", reply_to=isReplyTo(message.id))
         else:
             await message.answer("Данного конфига не существует", reply_to=isReplyTo(message.id))
-        with open('config.ini', 'w') as configfile:
-            config.write(configfile)
-        await message.answer("Конфиг успешно изменён", reply_to=isReplyTo(message.id))
     else:
         await message.answer("Без админки хуй тебе", reply_to=isReplyTo(message.id))
